184bd6639a9105e76566ba80adfdb4681288d548
philip fraser <pr.fraser3@gmail.com>
2017-12-05 15:37:38 +1000

    fixed json response and added post request

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET auth page and set loggedIn to true. */
-router.get('/auth', function(req, res, next) {
-	if (!req.session.loggedIn) {
+// router.get('/auth', function(req, res, next) {
+// 	if (!req.session.loggedIn) {
+// 		req.session.loggedIn = new Date().toISOString();
+// 	}
+// 	res.json({
+// 		loggedIn: req.session.loggedIn || false
+// 	})
+// });
+
+router.post('/auth', function(req, res, next) {
+	const realPassword = 'heeyooooh'
+	if ( req.body.password === realPassword ) {
 		req.session.loggedIn = new Date().toISOString();
 	}
-  res.send(req.session);
+	res.json({
+		loggedIn: req.session.loggedIn || false
+	})
+});
+
+router.delete('/auth', function(req, res, next) {
+	if (req.session.loggedIn) {
+		req.session.loggedIn = null
+	}
+	res.json({
+		loggedIn: req.session.loggedIn || false
+	})
 });
 
 /* GET secure page and check if loggedIn is true. */
@@ -17,9 +38,13 @@ router.get('/secure', function(req, res, next) {
 	++req.session.secureVisits
 	if (req.session.loggedIn) {  
 		const dateLoggedIn = new Date(req.session.loggedIn)
-		res.send(req.session) 
+		res.json({
+			loggedIn: req.session.loggedIn || false
+		})
 	} else { 
-		res.send(req.session)
+		res.json({
+			loggedIn: req.session.loggedIn || false
+		})
 	}
 });
 
